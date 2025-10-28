00f5eb9c98fb982f921afd0658c380b7feb99f15
ccullinane <ccullinane@companieshouse.gov.uk>
2025-10-28 12:11:23 +0000

    Coindtional added to allow single and multi az deployment

--- groups/chips-fsx/fsx_filesystem.tf
+++ groups/chips-fsx/fsx_filesystem.tf
@@ -1,6 +1,6 @@
 resource "aws_fsx_ontap_file_system" "chips_fsx" {
   storage_capacity    = var.fsx_storage_capacity
-  subnet_ids          = [values(data.aws_subnet.storage_subnet)[0].id, values(data.aws_subnet.storage_subnet)[1].id]
+  subnet_ids          = can(regex("MULTI", var.fsx_deployment_type)) ? [values(data.aws_subnet.storage_subnet)[0].id, values(data.aws_subnet.storage_subnet)[1].id] : [values(data.aws_subnet.storage_subnet)[0].id]
   deployment_type     = var.fsx_deployment_type
   throughput_capacity = var.fsx_throughput_capacity
   preferred_subnet_id = values(data.aws_subnet.storage_subnet)[0].id
