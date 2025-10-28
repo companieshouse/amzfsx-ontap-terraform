77c2fabb43df1d86f2a6c9fcf54c0c0f623fcba7
ccullinane <ccullinane@companieshouse.gov.uk>
2025-10-28 12:47:52 +0000

    volume name updated

--- groups/chips-fsx/fsx_volume.tf
+++ groups/chips-fsx/fsx_volume.tf
@@ -1,6 +1,6 @@
 resource "aws_fsx_ontap_volume" "chips_vol_01" {
-  name                       = "chips-vol-01"
-  junction_path              = "/chips-vol-01"
+  name                       = "chips_vol_01"
+  junction_path              = "/chips_vol_01"
   size_in_megabytes          = 1024
   storage_efficiency_enabled = true
   storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_svm.id
