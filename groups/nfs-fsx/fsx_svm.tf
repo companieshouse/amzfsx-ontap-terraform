0115b1de5de813f7d64eedc13c58c37e1b6500f5
ccullinane <ccullinane@companieshouse.gov.uk>
2025-11-10 16:56:43 +0000

    Updated svm name

--- groups/nfs-fsx/fsx_svm.tf
+++ groups/nfs-fsx/fsx_svm.tf
@@ -1,4 +1,4 @@
 resource "aws_fsx_ontap_storage_virtual_machine" "nfs_svm" {
   file_system_id = aws_fsx_ontap_file_system.nfs_fsx.id
-  name           = "chips_svm"
+  name           = "nfs_svm"
 }
