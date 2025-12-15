3bbdfe5488fe05775454d1a015da2281104377b7
ccullinane <ccullinane@companieshouse.gov.uk>
2025-12-15 14:17:43 +0000

    Use FSx filesystem mgmt address rather than svm

--- groups/chips-oltp-fsx/dns.tf
+++ groups/chips-oltp-fsx/dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "chips_oltp_fsx_dns" {
   zone_id = data.aws_route53_zone.private_zone.id
-  name    = var.fsx_dns_name
+  name    = var.fsx_fs_mgmt_dns_name
   type    = "CNAME"
   ttl     = 300
-  records = [aws_fsx_ontap_storage_virtual_machine.chips_oltp_svm.endpoints[0].management[0].dns_name]
+  records = [aws_fsx_ontap_file_system.chips_oltp_fsx.endpoints[0].management[0].dns_name]
 }
