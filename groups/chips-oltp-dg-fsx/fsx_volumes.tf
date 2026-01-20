6c1573e2251e4d2859fd3dcf02359405f62989f4
ccullinane <ccullinane@companieshouse.gov.uk>
2026-01-20 12:22:54 +0000

    added dataguard fsx

---
+++ groups/chips-oltp-dg-fsx/fsx_volumes.tf
@@ -0,0 +1,167 @@
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_01" {
+  name                       = "chips_oltp_dg_data_vol_01"
+  junction_path              = "/chips_oltp_dg_data_vol_01"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_02" {
+  name                       = "chips_oltp_dg_data_vol_02"
+  junction_path              = "/chips_oltp_dg_data_vol_02"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_03" {
+  name                       = "chips_oltp_dg_data_vol_03"
+  junction_path              = "/chips_oltp_dg_data_vol_03"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_04" {
+  name                       = "chips_oltp_dg_data_vol_04"
+  junction_path              = "/chips_oltp_dg_data_vol_04"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_05" {
+  name                       = "chips_oltp_dg_data_vol_05"
+  junction_path              = "/chips_oltp_dg_data_vol_05"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_data_vol_06" {
+  name                       = "chips_oltp_dg_data_vol_06"
+  junction_path              = "/chips_oltp_dg_data_vol_06"
+  size_in_megabytes          = var.chips_oltp_dg_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_fra_vol_01" {
+  name                       = "chips_oltp_dg_fra_vol_01"
+  junction_path              = "/chips_oltp_dg_fra_vol_01"
+  size_in_megabytes          = var.chips_oltp_dg_fra_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_fra_vol_02" {
+  name                       = "chips_oltp_dg_fra_vol_02"
+  junction_path              = "/chips_oltp_dg_fra_vol_02"
+  size_in_megabytes          = var.chips_oltp_dg_fra_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_fra_vol_03" {
+  name                       = "chips_oltp_dg_fra_vol_03"
+  junction_path              = "/chips_oltp_dg_fra_vol_03"
+  size_in_megabytes          = var.chips_oltp_dg_fra_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_fra_vol_04" {
+  name                       = "chips_oltp_dg_fra_vol_04"
+  junction_path              = "/chips_oltp_dg_fra_vol_04"
+  size_in_megabytes          = var.chips_oltp_dg_fra_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_redo_vol_01" {
+  name                       = "chips_oltp_dg_redo_vol_01"
+  junction_path              = "/chips_oltp_dg_redo_vol_01"
+  size_in_megabytes          = var.chips_oltp_dg_redo_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_redo_vol_02" {
+  name                       = "chips_oltp_dg_redo_vol_02"
+  junction_path              = "/chips_oltp_dg_redo_vol_02"
+  size_in_megabytes          = var.chips_oltp_dg_redo_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_redo_vol_03" {
+  name                       = "chips_oltp_dg_redo_vol_03"
+  junction_path              = "/chips_oltp_dg_redo_vol_03"
+  size_in_megabytes          = var.chips_oltp_dg_redo_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_dg_redo_vol_04" {
+  name                       = "chips_oltp_dg_redo_vol_04"
+  junction_path              = "/chips_oltp_dg_redo_vol_04"
+  size_in_megabytes          = var.chips_oltp_dg_redo_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_dg_svm.id
+
+  tiering_policy {
+    name = "NONE"
+  }
+}
