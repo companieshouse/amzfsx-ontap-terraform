1934c1f65fdd58d46dba478ebb1d0a37097645e5
ccullinane <ccullinane@companieshouse.gov.uk>
2026-05-15 15:09:54 +0100

    Added G2 FSx filesystems for CHIPS OLTP/DG

---
+++ groups/chips-oltp-dg-g2-fsx/iam.tf
@@ -0,0 +1,125 @@
+data "aws_iam_policy_document" "chips_oltp_dg_fsx" {
+  statement {
+    actions = [
+      "fsx:DescribeFileSystems",
+      "fsx:DescribeStorageVirtualMachines",
+      "fsx:DescribeVolumes",
+      "fsx:ListTagsForResource",
+      "fsx:DescribeBackups",
+      "fsx:DescribeSharedVpcConfiguration",
+      "cloudwatch:GetMetricData",
+      "cloudwatch:GetMetricStatistics",
+      "ec2:DescribeInstances",
+      "ec2:DescribeVolumes",
+      "elasticfilesystem:DescribeFileSystems",
+      "ce:GetCostAndUsage",
+      "ce:GetTags",
+      "ce:GetCostAndUsageWithResources",
+      "ce:GetCostForecast",
+      "ce:GetUsageForecast"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "fsx:CreateVolume",
+      "fsx:DeleteVolume",
+      "fsx:UpdateFileSystem",
+      "fsx:UpdateStorageVirtualMachine",
+      "fsx:UpdateVolume",
+      "fsx:CreateBackup",
+      "fsx:CreateVolumeFromBackup",
+      "fsx:DeleteBackup",
+      "fsx:TagResource",
+      "fsx:UntagResource",
+      "bedrock:InvokeModelWithResponseStream",
+      "bedrock:InvokeModel",
+      "bedrock:ListInferenceProfiles",
+      "bedrock:GetInferenceProfile"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "fsx:CreateFileSystem",
+      "fsx:CreateStorageVirtualMachine",
+      "fsx:DeleteFileSystem",
+      "fsx:DeleteStorageVirtualMachine",
+      "fsx:TagResource",
+      "fsx:UntagResource",
+      "kms:CreateGrant",
+      "iam:CreateServiceLinkedRole",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateTags",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeVolumeStatus",
+      "kms:DescribeKey",
+      "kms:ListKeys",
+      "kms:ListAliases"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:DeleteSecurityGroup"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "ec2:ResourceTag/AppCreator"
+      values   = ["NetappFSxWF"]
+    }
+  }
+
+  statement {
+    actions = [
+      "iam:SimulatePrincipalPolicy"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "chips_oltp_dg_fsx" {
+  name        = "chips_oltp_dg_fsx"
+  description = "Allows management of FSx storage workloads"
+  policy      = data.aws_iam_policy_document.chips_oltp_dg_fsx.json
+}
+
+resource "aws_iam_role" "chips_oltp_dg_fsx_role" {
+  name = "chips_oltp_dg_fsx_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "AWS" : local.netapp_fsx_account_id
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "sts:ExternalId" : local.netapp_account_id
+          }
+        }
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "attach_chips_oltp_dg_fsx_policy" {
+  role       = aws_iam_role.chips_oltp_dg_fsx_role.name
+  policy_arn = aws_iam_policy.chips_oltp_dg_fsx.arn
+}
---
+++ groups/chips-oltp-g2-fsx/fsx_volumes.tf
@@ -0,0 +1,90 @@
+resource "aws_fsx_ontap_volume" "chips_oltp_data_vol" {
+  count                      = var.chips_oltp_data_count
+  name                       = "chips_oltp_data_vol_${format("%02d", count.index + 1)}"
+  junction_path              = "/chips_oltp_data_vol_${format("%02d", count.index + 1)}"
+  size_in_megabytes          = var.chips_oltp_data_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_svm.id
+
+
+  tiering_policy {
+    name = "NONE"
+  }
+
+  tags = merge(
+    local.default_tags,
+    tomap(
+      {
+        "Volume" = "CHIPS-OLTP-G2-DATA"
+      }
+    )
+  )
+
+  lifecycle {
+    ignore_changes = [
+      size_in_megabytes,
+      junction_path
+    ]
+  }
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_fra_vol" {
+  count                      = var.chips_oltp_fra_count
+  name                       = "chips_oltp_fra_vol_${format("%02d", count.index + 1)}"
+  junction_path              = "/chips_oltp_fra_vol_${format("%02d", count.index + 1)}"
+  size_in_megabytes          = var.chips_oltp_fra_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_svm.id
+
+
+  tiering_policy {
+    name = "NONE"
+  }
+
+  tags = merge(
+    local.default_tags,
+    tomap(
+      {
+        "Volume" = "CHIPS-OLTP-G2-FRA"
+      }
+    )
+  )
+
+  lifecycle {
+    ignore_changes = [
+      size_in_megabytes,
+      junction_path
+    ]
+  }
+  
+}
+
+resource "aws_fsx_ontap_volume" "chips_oltp_redo_vol" {
+  count                      = var.chips_oltp_redo_count
+  name                       = "chips_oltp_redo_vol_${format("%02d", count.index + 1)}"
+  junction_path              = "/chips_oltp_redo_vol_${format("%02d", count.index + 1)}"
+  size_in_megabytes          = var.chips_oltp_redo_size
+  storage_efficiency_enabled = true
+  storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.chips_oltp_svm.id
+
+
+  tiering_policy {
+    name = "NONE"
+  }
+
+  tags = merge(
+    local.default_tags,
+    tomap(
+      {
+        "Volume" = "CHIPS-OLTP-G2-REDO"
+      }
+    )
+  )
+
+  lifecycle {
+    ignore_changes = [
+      size_in_megabytes,
+      junction_path
+    ]
+  }
+}
