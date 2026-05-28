e8c824db7b2dc4b8e6e84cc0e25d07467275e8bf
ccullinane <ccullinane@companieshouse.gov.uk>
2026-05-28 08:54:27 +0100

    removed unused groups

--- groups/chips-oltp-dg-fsx/README.md
+++
@@ -1,2 +0,0 @@
-# chips-fsx - FSX storage for CHIPS
-Terraform for provisioning the FSX storage for CHIPS
--- groups/chips-oltp-dg-fsx/fsx_svm.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_fsx_ontap_storage_virtual_machine" "chips_oltp_dg_svm" {
-  file_system_id = aws_fsx_ontap_file_system.chips_oltp_dg_fsx.id
-  name           = "chips_oltp_dg_svm"
-}
