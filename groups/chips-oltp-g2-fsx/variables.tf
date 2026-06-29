c6ec2ddcbfffc33f8f290ec1da3a74ea5a4ad0e0
ccullinane <ccullinane@companieshouse.gov.uk>
2026-06-29 12:45:33 +0100

    Updated cloudwatch metrics

--- groups/chips-oltp-g2-fsx/variables.tf
+++ groups/chips-oltp-g2-fsx/variables.tf
@@ -94,21 +94,21 @@ variable "fsx_fs_mgmt_dns_record" {
 }
 
 variable "chips_oltp_g2_data_size" {
-  type = string
+  type        = string
   description = "Volume Size in Mb"
-  default = "1"
+  default     = "1"
 }
 
 variable "chips_oltp_g2_fra_size" {
-  type = string
+  type        = string
   description = "Volume Size in Mb"
-  default = "1"
+  default     = "1"
 }
 
 variable "chips_oltp_g2_redo_size" {
-  type = string
+  type        = string
   description = "Volume Size in Mb"
-  default = "1"
+  default     = "1"
 }
 
 variable "chips_oltp_g2_data_count" {
